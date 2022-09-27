3513ec918b68fb50dc6f0f0c9ebd0cd37c3dd9f4
Jakub Samul <jakubsamul0@gmail.com>
2022-09-27 14:16:08 +0200

    3 filmik panel logowania i rejestracji

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
 from turtle import title
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -33,15 +33,24 @@ def about():
     return render_template("about.html", title="About")
 
 
-@app.route("/reginstert")
+@app.route("/register", methods=["GET", "POST"])
 def reginster():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
     return render_template("reginster.html", title="Reginster", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
     return render_template("login.html", title="Login", form=form)
 
 
